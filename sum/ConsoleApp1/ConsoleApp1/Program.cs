7a9ae7067bb2e6f391db99f1f56560201a6924db
Mahmoud Khademi <mamadztu@gmail.com>
2023-02-25 08:36:32 -0800

    add sum

---
+++ sum/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project1
+{
+    class App
+    {
+        static void Main()
+        {
+            Console.WriteLine("adad 1 ");
+            string num1= Console.ReadLine();
+            Console.WriteLine("adad 2 ");
+            string num2= Console.ReadLine();
+            int num1n = Convert.ToInt32(num1);
+            int num2n = Convert.ToInt32(num2);
+            Console.WriteLine("Jam " + num2n+ num1n);
+        }
+    }
+}
