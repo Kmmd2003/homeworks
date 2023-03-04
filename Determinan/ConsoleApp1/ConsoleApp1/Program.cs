31221be14b8070a2c98cb32353c2ef8e35e01a5e
Mahmoud Khademi <mamadztu@gmail.com>
2023-03-04 08:43:44 -0800

    fix names

--- Determinan/ConsoleApp1/ConsoleApp1/Program.cs
+++ Determinan/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 
-namespace App
+namespace Project1
 {
-    class Project
+    class App
     {
         static void Main()
         {
-            int[,] matrix = new int[3, 3];
+            int[,] array = new int[3, 3];
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
 
-                int j = (matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1])))- (matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0])))+ (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0])));
+                int j = (array[0, 0] * ((array[1, 1] * array[2, 2]) - (array[1, 2] * array[2, 1])))- (array[0, 1] * ((array[1, 0] * array[2, 2]) - (array[1, 2] * array[2, 0])))+ (array[0, 2] * ((array[1, 0] * array[2, 1]) - (array[1, 1] * array[2, 0])));
                 Console.WriteLine("matrix : " + j);
             }
         }
