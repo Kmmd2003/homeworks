e38eb1dbfde11a01c5a8e9fff58337125f8c83c4
Mahmoud Khademi <mamadztu@gmail.com>
2023-03-04 11:12:04 -0800

    deter2

--- deter2/ConsoleApp1/ConsoleApp1/Program.cs
+++ deter2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace App
+{
+    class Project
+    {
+        static void Main()
+        {
+            int[,] arr = new int[3, 3];
+            int num = 0;
+            int num2 = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Enter ["+ i+ "]["+ j+ "]: ");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Enter duplicate number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr[j, i] == num2)
+                        ++num;
+                }
+            }
+
+            Console.WriteLine("Duplicate count for "+ num2+ "is : "+ num);
+        }
+
+    }
+}
